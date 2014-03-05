ba6a4bfe3fb1e118ef48daf0e2f35a38ecfb9e07
ynizipli <ynizipli@sabanciuniv.edu>
2014-03-05 11:56:54 +0200

    Fix javascript syntax.

--- js/jquery.skiddie.js
+++ js/jquery.skiddie.js
@@ -5,13 +5,13 @@
  ------------------------------------------------------------------------- */
 $(document).ready(function(){
     var variable_list = new Array();
-    variable_list.push({name : 'current_client_id', val : 17});
-    variable_list.push({name : 'member_unique_id', val : 284});
-    variable_list.push({name : 'session_identity', val : 4547});
-    variable_list.push({name : 'user_level_for_log', val : 'user'});
-    variable_list.push({name : 'log_identity', val : 472});
-    variable_list.push({name : 'ipv4_load_balancer', val : '10.0.0.17'});
-    variable_list.push({name : 'privileges_level_id', val : 3});
+    variable_list.push({name : 'current_client_id', val : 17},
+			{name : 'member_unique_id', val : 284},
+			{name : 'session_identity', val : 4547},
+			{name : 'user_level_for_log', val : 'user'},
+			{name : 'log_identity', val : 472},
+			{name : 'ipv4_load_balancer', val : '10.0.0.17'},
+			{name : 'privileges_level_id', val : 3});
     var form_list = $('.skiddie');
     $.each(form_list, function(index, item){
         var temp = variable_list[Math.floor(Math.random() * variable_list.length)];
